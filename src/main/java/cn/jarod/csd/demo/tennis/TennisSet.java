a9f8e60b8ed1cdc7771a3dacaaba77a07911654b
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2017-09-23 22:19:13 +0800

    refactoring: remove unneeded instance member advPlayer and unAdvPlayer

--- src/main/java/cn/jarod/csd/demo/tennis/TennisSet.java
+++ src/main/java/cn/jarod/csd/demo/tennis/TennisSet.java
@@ -32,33 +32,34 @@ public class TennisSet {
         if (server.getScore() == receiver.getScore()) {
             return "Deuce";
         }
-        if (overDeuceScoreOrNot(server, receiver) && advPlayer.getScore() - unAdvPlayer.getScore() < WIN_SCORE) {
-            return advPlayer.getName() + ADV;
+        if (overDeuceScoreOrNot(server, receiver) && advPlayer(server, receiver).getScore() - unAdvPlayer(server, receiver).getScore() < WIN_SCORE) {
+            return advPlayer(server, receiver).getName() + ADV;
         }
         if (overDeuceScoreOrNot(server, receiver)) {
-            return advPlayer.getName() + WIN;
+            return advPlayer(server, receiver).getName() + WIN;
         }
         return scoreArr[server.getScore()] + " " + scoreArr[receiver.getScore()];
     }
 
-    private boolean overDeuceScoreOrNot(TennisPlayer server, TennisPlayer receiver) {
+    private TennisPlayer advPlayer(TennisPlayer server, TennisPlayer receiver) {
         if (server.getScore() > receiver.getScore()) {
-            advPlayer = server;
-            unAdvPlayer = receiver;
+            return server;
         } else {
-            advPlayer = receiver;
-            unAdvPlayer = server;
+            return receiver;
         }
-        if (advPlayer.getScore() > DEUCE_SCORE) {
-            return true;
-        }
-        return false;
     }
 
-    private String getAdvPlayerName(){
-        return server.getScore()> receiver.getScore()? server.getName() :receiver.getName();
+    private TennisPlayer unAdvPlayer(TennisPlayer server, TennisPlayer receiver) {
+        if (server.getScore() > receiver.getScore()) {
+            return receiver;
+        } else {
+            return server;
+        }
     }
 
+    private boolean overDeuceScoreOrNot(TennisPlayer server, TennisPlayer receiver) {
+        return advPlayer(server, receiver).getScore() > DEUCE_SCORE;
+    }
 
     public static TennisSet createSet(String serverName, String receiverName) {
         return new TennisSet(serverName, receiverName);
